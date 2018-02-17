be6ec40724cb28b8396db781afb1f226d294dbaa
pawgid <pgidaszewski@gmail.com>
2018-02-17 11:54:28 +0100

    first commit

---
+++ app/src/main/java/com/example/pawe/quiz/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pawe.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.welcomeTextView)
+    TextView welcomeTextView;
+    @BindView(R.id.welcomeButton)
+    Button welcomeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
+
---
+++ app/src/main/java/com/example/pawe/quiz/QuizList.java
@@ -0,0 +1,8 @@
+package com.example.pawe.quiz;
+
+/**
+ * Created by Paweł on 2018-02-17.
+ */
+
+public class QuizList {
+}
